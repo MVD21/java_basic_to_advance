134cea1248b45c3079c8ef91193a8e8c10dd08d7
Madhavi Vitthal Dongargave <madhavidongargave@gmail.com>
2026-01-11 20:08:20 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,22 @@
+class Operation
+{
+	public static void main(String args[])
+	{
+		int num1=20;
+		int num2=30;
+		int sum,multiply,division,subtract=0;
+		
+		sum=num1+num2;
+		System.out.println("Add two numbers :- "+sum);
+		
+		subtract=num2-num1;
+		System.out.println("Subtract two numbers :- "+subtract);
+
+		multiply=num1*num2;
+		System.out.println("Multiply two numbers :- "+multiply);
+
+		division=num1/num2;
+		System.out.println("Divide two numbers :- "+division);
+	}
+
+}
