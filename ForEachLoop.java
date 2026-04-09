5d57303b0dbb082025bb2f270ceca42549acbb02
Madhavi Vitthal Dongargave <madhavidongargave@gmail.com>
2026-04-09 19:20:41 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,10 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        // For each only traverse the array not modify the array element
+        int arr[]={10,20,30,60,70};
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+    
+}
